996e965e6927ce4b7c58d87ba428480ef443bb78
zhangzhaoyun <zhangzhaoyun@zailingtech.com>
2020-10-19 15:00:18 +0800

    [CHG] socket

--- ios/Classes/SwiftFlutterSocketPlugin.swift
+++ ios/Classes/SwiftFlutterSocketPlugin.swift
@@ -71,6 +71,8 @@ class FlutterSocket:NSObject, GCDAsyncSocketDelegate {
     /// 单例
     static let sharedInstance = FlutterSocket()
 
+    fileprivate var receiveBuf = Data(capacity: 1024*1024)
+
     /// 是否连接
     var connected:Bool = false
 
@@ -206,15 +208,52 @@ class FlutterSocket:NSObject, GCDAsyncSocketDelegate {
     ///   - tag: tag
     func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
         let dataCount = data.count
-        if(dataCount > 4 ){
-            let message = String(data: data.suffix(from: 4), encoding: String.Encoding.utf8)
-            if message != nil {
-                methodChannel.invokeMethod("receive_message", arguments: message)
-            }
+        let originMsg = String(data:data,encoding: String.Encoding.utf8)
+        if(dataCount>0) {
+            print("read messageLength :\(dataCount),message is : \(originMsg!)")
+
+            resolve(data: data)
         }
-        
 
         socket.readData(withTimeout: -1, tag: 0)
+    }
+
+    func resolve(data:Data) -> Void {
+//         print("read messageLength :\(dataCount),message is : \(originMsg!)")
+        receiveBuf.append(data)
+
+        while(true) {
+            let count = receiveBuf.count
+//            let bufferMsg = String(data:receiveBuf,encoding: String.Encoding.utf8)
+//            print("buffer length:  \(count) ,buffer msg:\(bufferMsg!)")
+            if(count > 4) {
+                let bodyLen = UInt32(receiveBuf[0]) << 24 + UInt32(receiveBuf[1]) << 16 + UInt32(receiveBuf[2]) << 8 + UInt32(receiveBuf[3])
+
+                let msgLength = Int(bodyLen)+4
+//                 print("bufferLenth : \(count), bodyLength : \(bodyLen), msgLength : \(msgLength)")
+                if( count >= msgLength ){
+                    let message = String(data: receiveBuf.subdata(in: 4..<msgLength),encoding: String.Encoding.utf8)
+//                    print("send message to flutter : \(message!)")
+                    methodChannel.invokeMethod("receive_message", arguments: message)
+
+                    if(count > msgLength) {
+//                        let resetData = receiveBuf.subdata(in: msgLength ..< count)
+//                        receiveBuf.replaceSubrange(0 ..< resetData.count, with: resetData)
+                        receiveBuf.removeSubrange(0 ..< msgLength)
+                    }else if (count == msgLength) {
+                        receiveBuf.removeAll(keepingCapacity: false)
+                    } else {
+                        print("some else ")
+                    }
+//                    receiveBuf.removeSubrange(0 ..< msgLength)
+
+                } else {
+                    break
+                }
+            }else {
+                break
+            }
+        }
     }
 
     /// GCDAsyncSocket didDisconnect call back
@@ -226,6 +265,7 @@ class FlutterSocket:NSObject, GCDAsyncSocketDelegate {
         socket.delegate = nil
         socket = nil
         connected = false
+        receiveBuf.removeAll()
 
         methodChannel.invokeMethod("disconnect", arguments: "disconnected")
     }
